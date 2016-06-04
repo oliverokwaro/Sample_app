a5821728924463cad17069b29cae21bb5cdfe9c6
oliverokwaro <oliverokwaro@gmail.com>
2016-06-04 10:43:28 +0300

    Log in form

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,13 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+  	render 'new'
+  end
+
+  def destroy
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -13,6 +15,12 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get 'login' => 'session#new'
+
+  post 'login' => 'session#create'
+
+  delete 'logout'  => 'session#destroy'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
