7f1b835774fddc5a8caa9f1309a87437b4e8deef
oliverokwaro <oliverokwaro@gmail.com>
2016-07-16 10:06:25 +0300

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160615090300) do
 
   create_table "microposts", force: :cascade do |t|
